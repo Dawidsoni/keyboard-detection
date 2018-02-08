4fa49518859457917599710b47f77b55a90f1e09
Dawid Wegner <dawid.wegner@gmail.com>
2018-02-08 01:03:39 +0100

    added chunks extractor

--- letter_recognition/utils/plot.py
+++ letter_recognition/utils/plot.py
@@ -16,6 +16,12 @@ def save_generator_sample(image_generator, output_file, row_size=20):
     sample_data = np.concatenate(combined_data, axis=0)
     Image.fromarray(sample_data).save(output_file)
 
+def plot_image(image, plot_size=None):
+    if plot_size is not None:
+        plt.figure(figsize=(16, 12))
+    plt.imshow(image, cmap='gray', vmin=0, vmax=255)               
+    plt.show()
+
 def plot_images_with_output(image_data, output_data):    
     output_ind = np.argsort(output_data)
     for i in range(image_data.shape[0]):
@@ -23,8 +29,7 @@ def plot_images_with_output(image_data, output_data):
         print(encode_class(ind1), encode_class(ind2), encode_class(ind3))
         prob_func = (lambda x: np.exp(output_data[i, x]))
         print(prob_func(ind1), prob_func(ind2), prob_func(ind3))
-        plt.imshow(image_data[i, :, :, :].reshape(32, 32), cmap='gray', vmin=0, vmax=255)               
-        plt.show()
+        plot_image(image_data[i, :, :, :].reshape(32, 32))
 
 def plot_letter_above_threshold(image_data, output_data, letter, thres):
     output_ind = np.argsort(output_data)
@@ -34,8 +39,7 @@ def plot_letter_above_threshold(image_data, output_data, letter, thres):
             continue
         if np.exp(output_data[i, ind]) < thres:
             continue
-        plt.imshow(image_data[i, :, :, :].reshape(32, 32), cmap='gray', vmin=0, vmax=255)               
-        plt.show()
+        plot_image(image_data[i, :, :, :].reshape(32, 32))
     
 def plot_all_above_threshold(image_data, output_data, thres):
     for letter in list(string.ascii_uppercase + string.digits):
